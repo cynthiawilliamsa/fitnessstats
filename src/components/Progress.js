99f15095fd3f695d91a2822394148af8bd35de07
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-12-31 17:50:34 -0600

    fetch setup for progress page

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { Component } from "react";
+import "../App.css";
+import Paper from "@material-ui/core/Paper";
 
-class Progress extends React.Component {
-    render() {
-      return (
-        <div className="Progress">
-        <h2 style={{textAlign: 'center', margin: '0', color: "black"}}>Hello, Cynthia!</h2>       
-        
-        </div> 
-      );
+class Progress extends Component {
+  state = {
+    stats: {},
+    loading: true
+  };
+
+  componentWillMount() {
+    fetch("http://localhost:3002/newentry")
+      .then(res => res.json())
+      .then(data => {
+        //create var to hold all stats sorted newest to oldest
+        const allStats = data.sort((a, b) => {
+          return b.date > a.date;
+        });
+        console.log(allStats);
+        this.setState({
+          ...this.state,
+          stats: allStats,
+          loading: false
+        });
+      })
+      .catch(err => console.log(err));
+
+    // setTimeout(()=> this.setState({
+    //   ...this.state,
+    //   loading: false
+    // }), 1000)
+  }
+  render() {
+    if (this.state.loading) {
+      return <LoadingStats />;
+    } else {
+      return <ProgressStats />;
     }
   }
-  
-  export default Progress;
+}
+
+const LoadingStats = () => {
+  return <h2>Loading</h2>;
+};
+
+const ProgressStats = () => {
+  return (
+    <div className="Progress">
+      <h2 style={{ textAlign: "center", margin: "0", color: "white" }}>
+        Check out your progress, Julie!
+      </h2>
+    </div>
+  );
+};
+
+export default Progress;
